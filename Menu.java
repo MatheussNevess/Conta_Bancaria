7ee5369c1771eaec38aa7658346d5bb725f9ee24
Matheus_Neves <matheuswdn@gmail.com>
2024-06-12 15:07:45 -0300

    Atualizando Menu

--- Menu.java
+++ Menu.java
@@ -1,14 +1,22 @@
 package conta;
 
-import conta.util.Cores;
 import java.util.Scanner;
+import conta.util.Cores;
+import conta.model.Conta;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		// Teste da Classe Conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
 
-Scanner leia = new Scanner(System.in);
+		Scanner leia = new Scanner(System.in);
 		
 		int opcao;
 		
